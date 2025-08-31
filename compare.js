9c0d5b18a4d3f40885c58a144716c294b5c2164f
weeqeecombe50 <mynieagbona@hotmail.com>
2025-08-31 12:34:42 +0300

    feat: initial commit for compare.js

---
+++ compare.js
@@ -0,0 +1,17 @@
+const { exec } = require('child_process');
+
+const [,, commit1, commit2] = process.argv;
+
+if (!commit1 || !commit2) {
+  console.error('Bitte zwei Commits angeben.');
+  process.exit(1);
+}
+
+exec(`git diff ${commit1} ${commit2}`, (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Fehler beim Vergleichen: ${stderr}`);
+    return;
+  }
+  console.log(`Unterschiede zwischen ${commit1} und ${commit2}:
+${stdout}`);
+});
